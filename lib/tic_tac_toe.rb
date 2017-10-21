1ca5d99202e4803e9c6bee6201c4a541aeebe94b
Steven Senkowski <scsenkowski@gmail.com>
2017-10-21 23:19:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,9 +72,7 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.find do |win_combo|
       if @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[0]] == @board[win_combo[2]] && position_taken?(win_combo[1]) && @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X"
-        return "X"
       else @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[0]] == @board[win_combo[2]] && position_taken?(win_combo[1]) && @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
-        return "O"
       end
     end
   end

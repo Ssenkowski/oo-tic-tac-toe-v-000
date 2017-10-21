1c56d3efe51996b2ea55bae6afcef14468eac57f
Steven Senkowski <scsenkowski@gmail.com>
2017-10-21 23:35:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -109,11 +109,8 @@ class TicTacToe
         counter += 1
       end
        if won?
-          if winner == "X" 
+         winner == "X" || winner == "O"
          puts "Congratulations #{winner(@board)}!"
-          else winner == "O"
-         puts "Congratulations #{winner(@board)}!"
-          end
        else draw?
          puts "Cat's Game!"
        end

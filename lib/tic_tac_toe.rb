2f87993cc2aa0b89b79de93c316841b17a4bfe55
Steven Senkowski <scsenkowski@gmail.com>
2017-10-21 22:13:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,10 @@ class TicTacToe
   end
 
 
+  def display_board=(board)
+    @board = board
+  end
+
   def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
